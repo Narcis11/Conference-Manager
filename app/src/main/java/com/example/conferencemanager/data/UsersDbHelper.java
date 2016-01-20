14c980204a77c3a2d8b69f8fb02c6627748f808a
Narcis Mihai <narcismihai11@gmail.com>
2016-01-20 19:11:55 +0200

    Added the CREATE statement for the Users table

---
+++ app/src/main/java/com/example/conferencemanager/data/UsersDbHelper.java
@@ -0,0 +1,34 @@
+package com.example.conferencemanager.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Narcis11 on 20/01/2016.
+ */
+public class UsersDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "users.db";
+
+    public UsersDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_USERS_TABLE = "CREATE TABLE " + UsersContract.UsersEntry.TABLE_NAME + " (" +
+                UsersContract.UsersEntry._ID + " INTEGER PRIMARY KEY, " +
+                UsersContract.UsersEntry.COLUMN_USERNAME + " TEXT, " +
+                UsersContract.UsersEntry.COLUMN_PASSWORD + " TEXT, " +
+                UsersContract.UsersEntry.COLUMN_USER_TYPE + " TEXT " + ");";
+
+        db.execSQL(SQL_CREATE_USERS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
