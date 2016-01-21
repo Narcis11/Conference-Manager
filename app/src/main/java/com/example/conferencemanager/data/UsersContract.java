30daceeae1b7c1ebc40299b8a01aca61ccac62f1
Narcis Mihai <narcismihai11@gmail.com>
2016-01-21 13:38:50 +0200

    Added the structure of the CONFERENCES table.

--- app/src/main/java/com/example/conferencemanager/data/UsersContract.java
+++ app/src/main/java/com/example/conferencemanager/data/UsersContract.java
@@ -12,6 +12,7 @@ public class UsersContract {
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     //used for determining which path to choose
     public static final String PATH_USERS = "users";
+    public static final String PATH_CONFERENCES = "conferences";
 
     public static final class UsersEntry implements BaseColumns {
         public static final String TABLE_NAME = "users";
@@ -29,6 +30,22 @@ public class UsersContract {
         public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_USERS;
         //used for manipulating a single row
         public static Uri buildUsersUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class ConferencesEntry implements BaseColumns {
+        public static final String TABLE_NAME = "conferences";
+        public static final String COLUMN_TITLE = "title";//text
+        public static final String COLUMN_DESCRIPTION = "description";//text
+        public static final String COLUMN_ADDRESS = "address";//text
+        public static final String COLUMN_DATE = "date";//text
+        public static final String COLUMN_ADDED_BY = "added_by";//text - the username of the person who added the conference
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_CONFERENCES).build();
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_CONFERENCES;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_CONFERENCES;
+        public static Uri buildConferencesUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
     }
