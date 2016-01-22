1c11852186785365135b711b88ead1c6be037e4e
Narcis Mihai <narcismihai11@gmail.com>
2016-01-22 12:55:51 +0200

    Added the topics layout elements

--- app/src/main/java/com/example/conferencemanager/admin/AdminTopicsFragment.java
+++ app/src/main/java/com/example/conferencemanager/admin/AdminTopicsFragment.java
@@ -11,22 +11,9 @@ import android.view.ViewGroup;
 import com.example.conferencemanager.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AdminTopicsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AdminTopicsFragment#newInstance} factory method to
- * create an instance of this fragment.
+ Used to display the available topics
  */
 public class AdminTopicsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,13 +21,6 @@ public class AdminTopicsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment AdminTopicsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AdminTopicsFragment newInstance() {
         AdminTopicsFragment fragment = new AdminTopicsFragment();
         return fragment;
@@ -49,10 +29,6 @@ public class AdminTopicsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,13 +36,6 @@ public class AdminTopicsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.admin_fragment_topics, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
@@ -86,16 +55,6 @@ public class AdminTopicsFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
