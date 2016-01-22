88abafb8baf534330e429ee76d8b06cd6e492f3b
Narcis Mihai <narcismihai11@gmail.com>
2016-01-22 07:56:11 +0200

    After the date is set, launch the dialog to set the hour and minute

--- app/src/main/java/com/example/conferencemanager/admin/AdminConferenceDetailsActivity.java
+++ app/src/main/java/com/example/conferencemanager/admin/AdminConferenceDetailsActivity.java
@@ -1,5 +1,7 @@
 package com.example.conferencemanager.admin;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,11 +12,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.conferencemanager.R;
 import com.example.conferencemanager.utilities.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AdminConferenceDetailsActivity extends AppCompatActivity{
 
@@ -33,7 +41,7 @@ public class AdminConferenceDetailsActivity extends AppCompatActivity{
     private EditText mTitleEditText;
     private EditText mAddressEditText;
     private EditText mDescriptionEditText;
-    private EditText mDateEditText;
+    private TextView mDateEditText;
     //the initial edittexts values
     private String mTitleInitialValue;
     private String mAddressInitialValue;
@@ -62,7 +70,7 @@ public class AdminConferenceDetailsActivity extends AppCompatActivity{
         mTitleEditText = (EditText) findViewById(R.id.admin_conf_details_title_value);
         mAddressEditText = (EditText) findViewById(R.id.admin_conf_details_address_value);
         mDescriptionEditText = (EditText) findViewById(R.id.admin_conf_details_description_value);
-        mDateEditText = (EditText) findViewById(R.id.admin_conf_details_date_value);
+        mDateEditText = (TextView) findViewById(R.id.admin_conf_details_date_value);
         //set the texts
         mTitleEditText.setText(mBundle.getString(Constants.BUNDLE_ADMIN_CONF_TITLE_KEY));
         mAddressEditText.setText(mBundle.getString(Constants.BUNDLE_ADMIN_CONF_ADDRESS_KEY));
@@ -87,54 +95,120 @@ public class AdminConferenceDetailsActivity extends AppCompatActivity{
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        Log.i(LOG_TAG,"In onOptionsItemSelected");
+        Log.i(LOG_TAG, "In onOptionsItemSelected");
         if (id == R.id.action_edit_conf) {
             Log.i(LOG_TAG, "Clicked on the edit button");
             if (!mIsEditModeEnabled) {
-                //enable all the edittexts
-                mTitleEditText.setEnabled(true);
-                mAddressEditText.setEnabled(true);
-                mDescriptionEditText.setEnabled(true);
-                mDateEditText.setEnabled(true);
-                //change the icons
-                mMenu.getItem(0).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_remove));//the left icon
-                mMenu.getItem(1).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_accept));//the right icon
-                mIsEditModeEnabled = true;
+                enableEditableMode();
             }
+            /*REMOVE*/
             else {//we are in editable mode, and the "remove" button is pressed
-                mMenu.getItem(0).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_edit));//the left icon
-                mMenu.getItem(1).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_delete));//the right icon
-                //set the edittexts back to their initial values
-                mTitleEditText.setText(mTitleInitialValue);
-                mAddressEditText.setText(mAddressInitialValue);
-                mDescriptionEditText.setText(mDescriptionInitialValue);
-                mDateEditText.setText(mDateInitialValue);
-                //disable the edittexts
-                mTitleEditText.setEnabled(false);
-                mAddressEditText.setEnabled(false);
-                mDescriptionEditText.setEnabled(false);
-                mDateEditText.setEnabled(false);
-
-                mIsEditModeEnabled = false;
+                onRemoveButtonPressed();
             }
         }
         else if (id == R.id.action_delete_conf) {
             Log.i(LOG_TAG,"Clicked on the delete button");
+            /*ACCEPT*/
             if (mIsEditModeEnabled) {//we are in editable mode, and the "accept" button is pressed
-                Log.i(LOG_TAG,"Ready to save the changes");
-                //change the icons back
-                mMenu.getItem(0).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_edit));//the left icon
-                mMenu.getItem(1).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_delete));//the right icon
-                //disable the edittexts
-                mTitleEditText.setEnabled(false);
-                mAddressEditText.setEnabled(false);
-                mDescriptionEditText.setEnabled(false);
-                mDateEditText.setEnabled(false);
-
-                mIsEditModeEnabled = false;
+                onAcceptButtonPressed();
             }
         }
         return super.onOptionsItemSelected(item);
     }
 
+    /**********************************************************START OF EDIT METHODS****************************************************************/
+    private void enableEditableMode() {
+        Log.i(LOG_TAG, "In enableEditableMode");
+        //enable all the edittexts
+        mTitleEditText.setEnabled(true);
+        mAddressEditText.setEnabled(true);
+        mDescriptionEditText.setEnabled(true);
+        mDateEditText.setEnabled(true);
+        //in addition, the date edittext also has to be clickable to trigger the calendar display
+        //mDateEditText.setClickable(true);
+        //change the icons
+        mMenu.getItem(0).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_remove));//the left icon
+        mMenu.getItem(1).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_accept));//the right icon
+        //set the logic for the date field
+        final Calendar conferenceCalendar = Calendar.getInstance();
+
+        final TimePickerDialog.OnTimeSetListener timePickerDialog = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                conferenceCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                conferenceCalendar.set(Calendar.MINUTE, minute);
+                updateConfCalendar(conferenceCalendar);
+            }
+        };
+
+        final DatePickerDialog.OnDateSetListener datePickerDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                conferenceCalendar.set(Calendar.YEAR, year);
+                conferenceCalendar.set(Calendar.MONTH, monthOfYear);
+                conferenceCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                //after the date is set, launch the dialog to set the hour and minute
+                new TimePickerDialog(AdminConferenceDetailsActivity.this, timePickerDialog, conferenceCalendar.get(Calendar.HOUR_OF_DAY),
+                        conferenceCalendar.get(Calendar.MINUTE), true).show();
+            }
+        };
+
+        mDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOG_TAG, "In mDateEditText onClickListener");
+                new DatePickerDialog(AdminConferenceDetailsActivity.this, datePickerDialog, conferenceCalendar
+                        .get(Calendar.YEAR), conferenceCalendar.get(Calendar.MONTH),
+                        conferenceCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        //the mode has changed
+        mIsEditModeEnabled = true;
+    }
+
+    private void onRemoveButtonPressed() {
+        mMenu.getItem(0).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_edit));//the left icon
+        mMenu.getItem(1).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_delete));//the right icon
+        //set the edittexts back to their initial values
+        mTitleEditText.setText(mTitleInitialValue);
+        mAddressEditText.setText(mAddressInitialValue);
+        mDescriptionEditText.setText(mDescriptionInitialValue);
+        mDateEditText.setText(mDateInitialValue);
+        //disable the edittexts
+        mTitleEditText.setEnabled(false);
+        mAddressEditText.setEnabled(false);
+        mDescriptionEditText.setEnabled(false);
+        mDateEditText.setEnabled(false);
+
+        mIsEditModeEnabled = false;
+    }
+
+    private void onAcceptButtonPressed() {
+        Log.i(LOG_TAG, "Ready to save the changes");
+        //change the icons back
+        mMenu.getItem(0).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_edit));//the left icon
+        mMenu.getItem(1).setIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_delete));//the right icon
+        //set the new edittexts values
+        mTitleEditText.setText(mTitleEditText.getText().toString());
+        mAddressEditText.setText(mAddressEditText.getText().toString());
+        mDescriptionEditText.setText(mDescriptionEditText.getText().toString());
+        mDateEditText.setText(mDateEditText.getText().toString());
+        //disable the edittexts
+        mTitleEditText.setEnabled(false);
+        mAddressEditText.setEnabled(false);
+        mDescriptionEditText.setEnabled(false);
+        mDateEditText.setEnabled(false);
+
+        mIsEditModeEnabled = false;
+    }
+
+    private void updateConfCalendar(Calendar calendar) {
+        Log.i(LOG_TAG,"In updateConfCalendar");
+        String dateFormat = "HH:mm, MM/dd/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+
+        mDateEditText.setText(sdf.format(calendar.getTime()));
+    }
+    /**********************************************************END OF EDIT METHODS****************************************************************/
 }
